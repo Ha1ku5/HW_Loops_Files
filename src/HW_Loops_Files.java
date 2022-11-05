071e968109d0b30fe62de1b4b45263775fd16542
ha1ku5 <alexleatha@gmail.com>
2022-11-04 17:33:39 -0700

    11/4/22

--- src/HW_Loops_Files.java
+++ src/HW_Loops_Files.java
@@ -19,7 +19,7 @@ public class HW_Loops_Files {
         double midtermWeight = file.nextDouble();
         double finalExamWeight = file.nextDouble();
 
-        //prints first header, as incorporating this into the loop would create unnecessary problems
+        //prints header for first class
         int firstClassNo = file.nextInt();
         System.out.println("Grade Data For Class " + firstClassNo);
         System.out.println(" ID   Programs  Midterm  Final  Weighted Average  Programs grade");
@@ -28,49 +28,52 @@ public class HW_Loops_Files {
         //keeps a running total of number of students and total
         int noOfStudents = 0;
         double totalGrade = 0;
+
         while(file.hasNextInt()){ //while the file has more data, keep running
+            int iD = file.nextInt();
 
+            if(iD == 0) { //case for when the end of a class is reached
+                //varTest(totalGrade, noOfStudents);
+                System.out.printf("Class average: %.2f%n", totalGrade / noOfStudents);
+                if(!file.hasNextInt())
+                    System.exit(0);
+                int classNo = file.nextInt();
 
-                int iD = file.nextInt();
-                //case for when the end of a class is reached
-                if(iD == 0) { 
-                    //System.out.println(totalGrade); //test
-                    //System.out.println(noOfStudents); //test
-                    System.out.printf("Class average: %.2f%n", totalGrade/noOfStudents);
-                    if(!file.hasNextInt())
-                        System.exit(0);
-                    int classNo = file.nextInt();
+                //printing the header for a class, not the Java kind
+                System.out.println("\nGrade Data For Class " + classNo);
+                System.out.println(" ID   Programs  Midterm  Final  Weighted Average  Programs grade");
+                System.out.println(" --   --------  -------  -----  ----------------  --------------");
+                iD = file.nextInt();
 
-                    //printing the header for a class, not the Java kind
-                    System.out.println();
-                    System.out.println("Grade Data For Class " + classNo);
-                    System.out.println(" ID   Programs  Midterm  Final  Weighted Average  Programs grade");
-                    System.out.println(" --   --------  -------  -----  ----------------  --------------");
-                    iD = file.nextInt();
+                //resets variables used in total weighted class average calculation for next class
+                noOfStudents = 0;
+                totalGrade = 0;
+            }
+            //gets grades for a student from courseData.txt, and calculates weighted average
+            int programsGrade = file.nextInt();
+            int midtermGrade = file.nextInt();
+            int finalGrade = file.nextInt();
+            double average = (proWeight*programsGrade + midtermWeight*midtermGrade
+                    + finalExamWeight*finalGrade);
 
-                    //resets variables used in total weighted class average calculation for next class
-                    noOfStudents = 0;
-                    totalGrade = 0;
-                }
-                //gets grades for a student from the file
-                int programsGrade = file.nextInt();
-                int midtermGrade = file.nextInt();
-                int finalGrade = file.nextInt();
-                double average = (proWeight*programsGrade + midtermWeight*midtermGrade
-                        + finalExamWeight*finalGrade);
+            //adds weighted average for current student to total class grade
+            totalGrade += average;
 
-                //adds weighted average for current student to total class grade
-                totalGrade += average;
+            //defines if the student passed, set to fail if grade is below a 70%
+            String passFail = "Fail";
+            if(programsGrade >= 70)
+                passFail = "Pass";
 
-                //defines if the student passed, set to fail if grade is below a 70%
-                String passFail = "Fail";
-                if(programsGrade >= 70)
-                    passFail = "Pass";
-
-                //printing each line
-                System.out.printf("%d     %d       %d       %d        %.2f             %s   %n"
-                        , iD, programsGrade, midtermGrade, finalGrade, average , passFail);
-                noOfStudents++;
+            //printing the current student's data
+            System.out.printf("%d     %d       %d       %d        %.2f             %s   %n"
+                    , iD, programsGrade, midtermGrade, finalGrade, average , passFail);
+            noOfStudents++;
         }
+    }
+    //method used for testing, not used in final program
+    public static void varTest(double totalGrade, int noOfStudents){
+        System.out.println(totalGrade); //test
+        System.out.println(noOfStudents); //test
+        System.out.println("Reached the end of a class"); //test
     }
 }
